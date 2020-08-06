ea466c9782b2dedd5a68663680158e793cf6254c
htrix <htrixe@gmail.com>
2020-08-06 10:53:44 +0100

    Jobs merged

--- src/main/java/com/hctec/candidates/repository/JobRepository.java
+++ src/main/java/com/hctec/candidates/repository/JobRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface JobRepository extends JpaRepository<Job, Long> {
+    // Mines
     List<Job> findByPublished(boolean published);
     List<Job> findByTitleContaining(String title);
 }
