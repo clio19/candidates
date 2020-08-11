0d07627e771d64fbd5f7c9c6b7c8876506a5f510
htrix <>
2020-08-11 20:13:28 +0100

    Vue login added

---
+++ client/vueClient/src/main.js
@@ -0,0 +1,41 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import { router } from './router'
+
+import store from './store';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import VeeValidate from 'vee-validate';
+import Vuex from 'vuex';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import {
+  faHome,
+  faUser,
+  faUserPlus,
+  faSignInAlt,
+  faSignOutAlt
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faHome, faUser, faUserPlus, faSignInAlt, faSignOutAlt);
+
+Vue.config.productionTip = false;
+
+Vue.use(VeeValidate);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.use(Vuex);
+
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
